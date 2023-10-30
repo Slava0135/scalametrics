b873f8246f999a0fc09a0c0bc7a16196ab5a1dc3
Slava0135 <super.novalskiy_0135@inbox.ru>
2023-10-30 13:21:27 +0300

    DepthOfInheritance setup

---
+++ src/main/scala/metrics/DepthOfInheritance.scala
@@ -0,0 +1,8 @@
+package io.github.slava0135.scalametrics
+package metrics
+
+object DepthOfInheritance {
+  def evaluate(source: String): Double = {
+    1
+  }
+}
---
+++ src/test/scala/metrics/DepthOfInheritanceTest.scala
@@ -0,0 +1,18 @@
+package io.github.slava0135.scalametrics
+package metrics
+
+import org.scalatest.funsuite.AnyFunSuite
+
+import scala.io.Source
+
+class DepthOfInheritanceTest extends AnyFunSuite {
+
+  def readSource(name: String): String = {
+    Source.fromResource(s"metrics/DepthOfInheritance/$name.scala").getLines().mkString("\n")
+  }
+
+  test("DepthOfInheritance.evaluate.NoParent") {
+    val source = readSource("NoParent")
+    assert(DepthOfInheritance.evaluate(source) === 1)
+  }
+}

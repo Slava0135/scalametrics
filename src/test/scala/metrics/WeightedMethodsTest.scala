531bcc14fb52dabf55b30f57f6077154695943f7
Slava0135 <super.novalskiy_0135@inbox.ru>
2023-10-30 12:33:43 +0300

    read source function

--- src/test/scala/metrics/WeightedMethodsTest.scala
+++ src/test/scala/metrics/WeightedMethodsTest.scala
@@ -6,8 +6,13 @@ import org.scalatest.funsuite.AnyFunSuite
 import scala.io.Source
 
 class WeightedMethodsTest extends AnyFunSuite {
+
+  def readSource(name: String): String = {
+    Source.fromResource(s"metrics/WeightedMethods/$name.scala").getLines().mkString("\n")
+  }
+
   test("WeightedMethods.evaluate") {
-    val source = Source.fromResource("metrics/WeightedMethods/NoMethods.scala").getLines().mkString("\n")
+    val source = readSource("NoMethods")
     assert(WeightedMethods.evaluate(source) === 0)
   }
 }
